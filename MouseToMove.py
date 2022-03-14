6ccae996c81e3c476a224d38c2bf3f93fb272218
NikhilBajpai <nikhil.cse16@gmail.com>
2022-03-14 20:04:58 +0530

    to take screenshot

--- MouseToMove.py
+++ MouseToMove.py
@@ -1,22 +1,21 @@
-from functools import reduce
-
-import mouse
+import math
+import operator
 import time
-from pynput import keyboard
+from functools import reduce
+import mouse
 import pyautogui
 from PIL import Image
 from PIL import ImageChops
-import math, operator
+import firebase_admin
+from firebase_admin import credentials
 
-img1 = Image.open("Screenshot (6).png")
+img1 = Image.open(r"C:\Users\NikhilBajpai\Pictures\Screenshots\Screenshot (12).png")
 
-img2 = Image.open("Screenshot (7).png")
+# img2 = Image.open("Screenshot (7).png")
+img2 = Image.open(r"C:\Users\NikhilBajpai\Pictures\Screenshots\Screenshot (13).png")
 
 
-# def equal(img1, img2):
-#     print(ImageChops.difference(img1, img2).getbbox())
-#     return ImageChops.difference(img1, img2).getbbox() is None
-def rmsdiff(im1, im2):
+def rmsDiff(im1, im2):
     "Calculate the root-mean-square difference between two images"
 
     h = ImageChops.difference(im1, im2).histogram()
@@ -27,13 +26,22 @@ def rmsdiff(im1, im2):
                             ) / (float(im1.size[0]) * im1.size[1]))
 
 
-print(rmsdiff(img1, img2))
+print(rmsDiff(img1, img2))
+
+
+def TakeScreenShot():
+    pyautogui.hotkey("winleft", "prtsc")
 
 
 def mouseMovement():
     while True:
         mouse.move(1900, 350, absolute=True, duration=3)
+        rmsDiff()
         time.sleep(4)
-
+        print("hehehaha")
         mouse.move(2100, 350, absolute=True, duration=3)
         time.sleep(4)
+        print("hehehaha444444")
+
+
+print(mouseMovement())
